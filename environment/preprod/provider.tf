4acee6885c8f31dba08c06c9085236843b373ef1
Sudarshan Sutar <sudarshansutar1995@gmail.com>
2026-09-11 15:28:46 +0530

    added  backend information

--- environment/preprod/provider.tf
+++ environment/preprod/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.65.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-sudarshan"
+    storage_account_name = "straccountdev"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
